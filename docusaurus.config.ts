7a22b89addcb4b2c2ea0cbcad09c96618939f791
son-hyejun <h_jun0515@naver.com>
2026-01-26 09:58:14 +0900

    chore: Chage url to vercel

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,10 +15,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://son-hyejun.github.io',
+  url: 'https://introduction-to-codejam.vercel.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/codejam-site/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
